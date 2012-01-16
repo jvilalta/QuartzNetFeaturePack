e47eb04cfff0a2cb0d7dd9d1964492a194f34f2c
Jay Vilalta <jay.vilalta@gmail.com>
2012-01-16 15:45:39 -0600

    add the job

---
+++ Quartz.FeaturePack.Jobs/AddListenerJob.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Quartz.Listener;
+
+namespace Quartz.FeaturePack.Jobs
+{
+    public class AddListenerJob : IJob
+    {
+        public void Execute(IJobExecutionContext context)
+        {
+            string type = context.MergedJobDataMap.GetString("type");
+            IJobListener listener = (IJobListener)Type.GetType(type).GetConstructor(new Type[] { }).Invoke(null);
+            IMatcher<JobKey> matcher = Quartz.Impl.Matchers.EverythingMatcher<JobKey>.AllJobs();
+            context.Scheduler.ListenerManager.AddJobListener(listener, matcher);
+        }
+    }
+}
