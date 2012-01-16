b25dc7d576177f6c68f23bddad85234111780ab1
Jay Vilalta <jay.vilalta@gmail.com>
2012-01-16 15:16:58 -0600

    add stub file for xml loader

---
+++ Quartz.FeaturePack.Plugins/XmlLoaderPlugin.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Quartz.Spi;
+
+namespace Quartz.FeaturePack.Plugins
+{
+    /// <summary>
+    /// This plugin loads jobs, triggers, listeners and calendars that are defined in an xml file
+    /// </summary>
+    public class XmlLoaderPlugin : ISchedulerPlugin
+    {
+        public void Initialize(string pluginName, IScheduler sched)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Shutdown()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Start()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
