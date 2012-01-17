5a0c35d9f28c7cc0ec5923a36a89bbd374033392
Jay Vilalta <jay.vilalta@gmail.com>
2012-01-17 11:04:01 -0600

    add documentation

--- Quartz.FeaturePack.Jobs/AddListenerJob.cs
+++ Quartz.FeaturePack.Jobs/AddListenerJob.cs
@@ -6,11 +6,25 @@ using Quartz.Listener;
 
 namespace Quartz.FeaturePack.Jobs
 {
+    /// <summary>
+    /// This job adds a listener to the scheduler. Set the "type" property of the job to the Type of the listener 
+    /// that you want to add to the scheduler. This job is handy if you want to add a listener to the scheduler via 
+    /// the quartz_jobs.xml file. The listener will be added with an EverythingMatcher, so it will match all jobs.
+    /// </summary>
     public class AddListenerJob : IJob
     {
+        /// <summary>
+        /// Looks up the listener's <see cref="Type">Type</see> in the job's data map. You must set the value
+        /// of the listener's type in a parameter called "type".
+        /// </summary>
+        /// <param name="context"></param>
         public void Execute(IJobExecutionContext context)
         {
             string type = context.MergedJobDataMap.GetString("type");
+            if (string.IsNullOrEmpty(type))
+            {
+                throw new ArgumentNullException("type", "Could not find a parameter named type in the job's datamap.");
+            }
             IJobListener listener = (IJobListener)Type.GetType(type).GetConstructor(new Type[] { }).Invoke(null);
             IMatcher<JobKey> matcher = Quartz.Impl.Matchers.EverythingMatcher<JobKey>.AllJobs();
             context.Scheduler.ListenerManager.AddJobListener(listener, matcher);
