859d6d6d54f075f9043edc3497eb4d326b379459
Jay Vilalta <jay.vilalta@gmail.com>
2012-05-17 18:58:07 -0500

    replace most dependencies with nuget packages

--- Quartz.FeaturePack.CLI/Program.cs
+++ Quartz.FeaturePack.CLI/Program.cs
@@ -3,38 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Topshelf;
-using Quartz.Server;
+using Quartz.FeaturePack.Server;
 namespace Quartz.FeaturePack.CLI
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Host host = HostFactory.New(x =>
-            {
-                x.Service<IQuartzServer>(s =>
-                {
-                    s.SetServiceName("quartz.server");
-                    s.ConstructUsing(builder =>
-                    {
-                        QuartzServer server = new QuartzServer();
-                        server.Initialize();
-                        return server;
-                    });
-                    s.WhenStarted(server => server.Start());
-                    s.WhenPaused(server => server.Pause());
-                    s.WhenContinued(server => server.Resume());
-                    s.WhenStopped(server => server.Stop());
-                });
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Host host = HostFactory.New(x =>
+			{
+				x.Service<IQuartzServer>(s =>
+				{
+					s.SetServiceName("quartz.server");
+					s.ConstructUsing(builder =>
+					{
+						QuartzServer server = new QuartzServer();
+						server.Initialize();
+						return server;
+					});
+					s.WhenStarted(server => server.Start());
+					s.WhenPaused(server => server.Pause());
+					s.WhenContinued(server => server.Resume());
+					s.WhenStopped(server => server.Stop());
+				});
 
-                x.RunAsLocalSystem();
+				x.RunAsLocalSystem();
 
-                x.SetDescription(Configuration.ServiceDescription);
-                x.SetDisplayName(Configuration.ServiceDisplayName);
-                x.SetServiceName(Configuration.ServiceName);
-            });
+				x.SetDescription("Quartz.Net Server");
+				x.SetDisplayName("Quartz.Net Server");
+				x.SetServiceName("Quartz.Net Server");
+			});
 
-            host.Run();
-        }
-    }
+			host.Run();
+		}
+	}
 }
---
+++ Quartz.FeaturePack.Server/IQuartzServer.cs
@@ -0,0 +1,36 @@
+using System;
+
+namespace Quartz.FeaturePack.Server
+{
+	/// <summary>
+	/// Service interface for core Quartz.NET server.
+	/// </summary>
+	public interface IQuartzServer
+	{
+		/// <summary>
+		/// Initializes the instance of <see cref="IQuartzServer"/>.
+		/// Initialization will only be called once in server's lifetime.
+		/// </summary>
+		void Initialize();
+
+		/// <summary>
+		/// Starts this instance.
+		/// </summary>
+		void Start();
+
+		/// <summary>
+		/// Stops this instance.
+		/// </summary>
+		void Stop();
+
+		/// <summary>
+		/// Pauses all activity in scheudler.
+		/// </summary>
+		void Pause();
+
+		/// <summary>
+		/// Resumes all acitivity in server.
+		/// </summary>
+		void Resume();
+	}
+}
