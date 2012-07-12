c44645d6344a82d5440df3c41a3e6fcb0c298897
Jay Vilalta <jay.vilalta@gmail.com>
2012-07-12 17:28:47 -0500

    add DiagnosticJob. add CastleJobFactory

---
+++ Quartz.FeaturePack.JobFactories/CastleJobFactory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Quartz.Spi;
+using Castle.Windsor;
+using Castle.Windsor.Configuration.Interpreters;
+using Castle.Core.Resource;
+using Castle.MicroKernel.Registration;
+using Castle.Core;
+using System.Reflection;
+using System.IO;
+
+namespace Quartz.FeaturePack.JobFactories
+{
+	public class CastleJobFactory : IJobFactory
+	{
+		public String JobAssemblyDirectory { get; set; }
+		public CastleJobFactory()
+		{
+			_Container = new WindsorContainer(new XmlInterpreter(new ConfigResource("castle")));
+			string path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+			_Container.Register(AllTypes.FromAssemblyInDirectory(new AssemblyFilter(path)).BasedOn<IJob>().Configure(c => c.LifeStyle.Is(LifestyleType.Transient)));
+		}
+		public IJob NewJob(TriggerFiredBundle bundle, IScheduler scheduler)
+		{
+			return _Container.Resolve(bundle.JobDetail.JobType) as IJob;
+		}
+		private static WindsorContainer _Container;
+
+	}
+}
---
+++ Quartz.FeaturePack.Jobs/DiagnosticJob.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Common.Logging;
+
+namespace Quartz.FeaturePack.Jobs
+{
+	/// <summary>
+	/// This job dumps to the log all of the scheduler information as it is running. It gives us a look at what is going on at a given time.
+	/// </summary>
+	public class DiagnosticJob : IJob
+	{
+		#region IJob Members
+
+		public void Execute(IJobExecutionContext context)
+		{
+			_Log.InfoFormat("Calendar Names: {0}", string.Join(",", context.Scheduler.GetCalendarNames()));
+			outputMetadata(context.Scheduler.GetMetaData());
+		}
+
+		private void outputMetadata(SchedulerMetaData metaData)
+		{
+			_Log.InfoFormat("Scheduler Metadata:\nRunning Since: {0}", metaData.RunningSince);
+		}
+
+		#endregion
+		private static readonly ILog _Log = LogManager.GetLogger(typeof(DiagnosticJob));
+	}
+}
