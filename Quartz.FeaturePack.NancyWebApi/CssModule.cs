dfbcc741ad1868165f7db1d02e14ca1a31742fa5
Jay Vilalta <jay.vilalta@gmail.com>
2013-10-22 17:27:02 -0500

    add bootstrap

---
+++ Quartz.FeaturePack.NancyWebApi/CssModule.cs
@@ -0,0 +1,21 @@
+﻿using Nancy;
+using Nancy.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Quartz.FeaturePack.NancyWebApi
+{
+    public class CssModule : NancyModule
+    {
+        public CssModule()
+            : base("/css")
+        {
+            Get["/{script}"] = parameters =>
+            {
+                return new Nancy.Responses.EmbeddedFileResponse(GetType().Assembly, "Quartz.FeaturePack.NancyWebApi.Web.css", parameters.script);
+            };
+        }
+    }
+}
